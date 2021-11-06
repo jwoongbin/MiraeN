47f2da7aef451b842581e7c0d228c762c64c044f
DESKTOP-RHQTACD\User <philoleben@gmail.com>
2021-11-06 18:20:00 +0900

    롤백 커밋

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Mindmap from './components/Mindmap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
