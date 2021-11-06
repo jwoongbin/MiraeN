84cc58354f125ba7ef06abd61fec5de6ca884275
DESKTOP-RHQTACD\User <philoleben@gmail.com>
2021-11-06 17:15:16 +0900

    no message

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,11 +1,17 @@
-
+import './Quiz.css';
 
 function Quiz({data}){
   console.log(data[0].subs[0].questiones[0].num);
   return (
-    <div>{data.map((item, index) =>
-      data[0].subs[0].questiones[index].question
-      )}</div>
+    <div className="Quiz-wrap">
+      <div className="header"><h1>[개념 터치 추가 문제] -1</h1></div>
+      <div className="contents">
+        {data.map((item, index) =>
+        data[0].subs[0].questiones[index].question)}
+      </div>
+      <div className="footer"></div>
+    </div>
+    
   );
 }
 
